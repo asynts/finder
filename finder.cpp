73d197dfc9ebb0e96a20780c50a31e693a94dd59
asynts <asynts@gmail.com>
2020-01-06 17:41:57 +0100

    Why isn't `std::istream` moveable?

--- finder.cpp
+++ finder.cpp
@@ -10,12 +10,14 @@
 
 #include "sha.cpp"
 
+constexpr size_t finder_version = 0;
+
 struct encoder {
 private:
-    std::basic_ostream<char> &output;
+    std::ostream &output;
 
 public:
-    encoder(std::basic_ostream<char> &output)
+    encoder(std::ostream &output)
         : output(output) { }
 
     void write(std::size_t value) {
@@ -30,100 +32,40 @@ public:
     }
 };
 
-struct decoder {
-private:
-    std::basic_istream<char> &input;
-
-public:
-    decoder(std::basic_istream<char> &input)
-        : input(input) { }
-
-    void read(size_t &value) {
-        std::array<char, sizeof(std::size_t)> value_bytes;
-
-        input.read(value_bytes.data(), sizeof(std::size_t));
-        std::memcpy(&value, &value_bytes, sizeof(std::size_t));
-    }
-
-    void read(char *data, std::size_t size) {
-        input.read(data, size);
-    }
-
-    void seek(std::size_t offset) {
-        input.seekg(offset);
-    }
-};
-
-struct entry {
-    std::string filename;
-    std::array<char, 32> digest;
-
-    entry() {
-    }
-
-    entry(std::string_view filename, const std::array<char, 32> &digest)
-        : filename(filename), digest(digest) {
-    }
-
-    bool operator<(const entry &rhs) const noexcept {
-        for(std::size_t idx = 0; idx < 32; ++idx) {
-            if(digest[idx] < rhs.digest[idx]) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-};
-
 struct database {
-private:
-    std::vector<entry> entries;
-    SHA3_256 hasher;
+    struct entry {
+        std::string filename;
+        std::array<char, 32> digest;
 
-public:
-    static database load(std::basic_istream<char> &input) {
-        database db;
-        decoder dec{input};
-
-        std::size_t count;
-        dec.read(count);
-
-        db.entries.resize(count);
-
-        std::size_t offset = sizeof(std::size_t);
-        for(std::size_t idx = 0; idx < count; ++idx) {
-            entry &entry = db.entries[idx];
-
-            dec.seek(offset);
-            dec.read(entry.digest.data(), 32);
-
-            std::size_t reference;
-            dec.read(reference);
-
-            dec.seek(reference);
-
-            std::size_t length;
-            dec.read(length);
-
-            entry.filename.resize(length);
-            dec.read(entry.filename.data(), length);
-
-            offset += 32 + sizeof(size_t);
+        entry() {
         }
 
-        return db;
-    }
+        entry(std::string_view filename, const std::array<char, 32> &digest)
+            : filename(filename), digest(digest) {
+        }
 
-    void save(std::basic_ostream<char> &output) {
+        bool operator<(const entry &rhs) const noexcept {
+            for(std::size_t idx = 0; idx < 32; ++idx) {
+                if(digest[idx] < rhs.digest[idx]) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    };
+
+    template <typename Container>
+    static void marshall(Container &&entries, std::ostream &output) {
         encoder enc{output};
 
         // Sorted lists can be searched faster.
         std::sort(entries.begin(), entries.end());
 
         // This expression evaluates to the offset of the lookup table.
-        size_t offset = sizeof(size_t) + (32 + sizeof(size_t)) * entries.size();
+        size_t offset = 2 * sizeof(size_t) + (32 + sizeof(size_t)) * entries.size();
 
+        enc.write(finder_version);
         enc.write(entries.size());
 
         // Write the index table.
@@ -140,22 +82,80 @@ public:
             enc.write(entry.filename.data(), entry.filename.size());
         }
     }
+};
 
-    void add(std::string_view filename) {
-        std::array<char, 32> digest;
-        hasher.compute(filename, digest);
+struct decoder {
+private:
+    std::istream input;
 
-        entries.emplace_back(filename, digest);
+public:
+    decoder(std::istream &&input)
+        : input{std::move(input)} { }
+
+    void decode(size_t &value) {
+        std::array<char, sizeof(std::size_t)> value_bytes;
+
+        input.read(value_bytes.data(), sizeof(std::size_t));
+        std::memcpy(&value, &value_bytes, sizeof(std::size_t));
     }
 
-    std::vector<entry>& raw() {
-        return entries;
+    void decode(char *data, std::size_t size) {
+        input.read(data, size);
     }
 
-    const std::vector<entry>& raw() const {
-        return entries;
+    void seek(std::size_t offset) {
+        input.seekg(offset);
     }
 };
+
+struct lazy_database {
+    struct entry {
+        std::array<char, 32> digest;
+        size_t offset;
+    };
+
+    std::vector<entry> entries;
+    mutable decoder dec;
+
+    lazy_database(std::istream &&input)
+        : dec{std::move(input)}
+    {
+        size_t version;
+        dec.decode(version);
+
+        if(version != finder_version) {
+            throw std::runtime_error{"invalid or outdated file format"};
+        }
+
+        size_t count;
+        dec.decode(count);
+        entries.resize(count);
+
+        for(size_t idx=0; idx < count; ++idx) {
+            dec.decode(entries[idx].digest.data(), 32);
+            dec.decode(entries[idx].offset);
+        }
+    }
+
+    std::string lookup(size_t offset) const {
+        dec.seek(offset);
+
+        size_t length;
+        dec.decode(length);
+
+        std::string filename;
+        filename.resize(length);
+
+        dec.decode(filename.data(), length);
+
+        return filename;
+    }
+
+    std::vector<std::string> locate(std::string_view filename) const {
+        std::terminate(); // TODO
+    }
+};
+
 
 int main() {
 }
