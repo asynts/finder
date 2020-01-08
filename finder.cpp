6df86b8d47a034a18b1621abc64fec890ca2006f
asynts <asynts@gmail.com>
2020-01-08 19:21:30 +0100

    I recovered the sources, but `finder FILENAME` isn't working.

--- finder.cpp
+++ finder.cpp
@@ -1,4 +1,7 @@
+#include <algorithm>
+#include <array>
 #include <cstdlib>
+#include <cstring>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -6,8 +9,6 @@
 #include <vector>
 
 #include <fmt/format.h>
-
-#include "database.cpp"
 
 namespace fs = std::filesystem;
 
@@ -33,18 +34,162 @@ finder 0.0.1
 Copyright (C) 2020 Paul Scharnofske; Licensed under the terms of MIT.
 )";
 
-std::fstream open_database_output_stream(fs::path directory) {
-    const auto cache_filepath = directory / finder_cache_path;
+struct encoder {
+private:
+    std::ostream &output;
 
-    std::fstream stream{cache_filepath, std::ios::out};
-    encoder enc{stream};
+public:
+    encoder(std::ostream &output)
+        : output(output) { }
 
-    enc.write(finder_abi_version);
+    void write(std::size_t value) {
+        std::array<char, sizeof(value)> value_bytes;
+        std::memcpy(&value_bytes, &value, sizeof(value));
 
-    return stream; // CE
-}
+        output.write(value_bytes.data(), value_bytes.size());
+    }
 
-std::fstream open_database_input_stream(fs::path directory) {
+    void write(const char *data, std::size_t size) {
+        output.write(data, size);
+    }
+};
+
+struct database {
+private:
+    struct entry {
+        std::string path;
+        std::size_t digest;
+
+        bool operator<(size_t rhs) const noexcept {
+            return digest < rhs;
+        }
+    };
+
+    // Sorted lists can be searched faster, thus this list shall always be sorted.
+    std::vector<entry> entries;
+
+public:
+    void add(fs::path path) {
+        std::size_t digest = fs::hash_value(path.filename());
+
+        const auto iter = std::lower_bound(entries.begin(),
+                                           entries.end(),
+                                           digest);
+
+        entries.emplace(iter, entry{ path.native(), digest });
+    }
+
+    void marshall(std::ostream &output) {
+        encoder enc{output};
+
+        // This expression evaluates to the offset of the lookup table.
+        std::size_t offset = (1 + entries.size()) * 2 * sizeof(std::size_t);
+
+        enc.write(finder_abi_version);
+        enc.write(entries.size());
+
+        // Write the index table.
+        for(const auto &entry : entries) {
+            enc.write(entry.digest);
+            enc.write(offset);
+
+            offset += sizeof(size_t) + entry.path.size();
+        }
+
+        // Write the lookup table.
+        for(const auto &entry : entries) {
+            enc.write(entry.path.size());
+            enc.write(entry.path.data(), entry.path.size());
+        }
+    }
+};
+
+struct decoder {
+private:
+    std::istream &input;
+
+public:
+    decoder(std::istream &input)
+        : input{input} { }
+
+    void decode(size_t &value) {
+        std::array<char, sizeof(std::size_t)> value_bytes;
+
+        input.read(value_bytes.data(), sizeof(std::size_t));
+        std::memcpy(&value, &value_bytes, sizeof(std::size_t));
+    }
+
+    void decode(char *data, std::size_t size) {
+        input.read(data, size);
+    }
+
+    void seek(std::size_t offset) {
+        input.seekg(offset);
+    }
+};
+
+// This database is lazy because only the hashes are decoded, and the paths are extracted on demand.
+struct lazy_database {
+    struct entry {
+        std::size_t digest;
+        std::size_t offset;
+
+        bool operator<(std::size_t rhs) const noexcept {
+            return digest < rhs;
+        }
+    };
+
+private:
+    std::vector<entry> entries;
+    mutable decoder dec;
+
+public:
+    lazy_database(std::istream &input)
+        : dec{input}
+    {
+        // Notice: It is expected, that the ABI version was already consumed and validated.
+
+        std::size_t count;
+        dec.decode(count);
+        entries.resize(count);
+
+        for(std::size_t idx=0; idx < count; ++idx) {
+            dec.decode(entries[idx].digest);
+            dec.decode(entries[idx].offset);
+        }
+    }
+
+    fs::path lookup(std::size_t offset) const {
+        dec.seek(offset);
+
+        std::size_t length;
+        dec.decode(length);
+
+        std::string path;
+        path.resize(length);
+        dec.decode(path.data(), length);
+
+        return path;
+    }
+
+    std::vector<fs::path> locate(fs::path filename) const {
+        std::vector<fs::path> matches;
+
+        const size_t digest = hash_value(filename);
+
+        auto iter = std::lower_bound(entries.begin(), entries.end(), digest);
+
+        while(iter != entries.end() && iter->digest == digest) {
+            matches.push_back( lookup(iter->offset) );
+
+            ++iter;
+        }
+
+        return matches;
+    }
+};
+
+std::fstream open_database_stream(fs::path directory) {
     const auto cache_filepath = directory / finder_cache_path;
 
     if(not fs::exists(cache_filepath)) {
@@ -53,15 +198,6 @@ std::fstream open_database_input_stream(fs::path directory) {
     }
 
     std::fstream stream{cache_filepath, std::ios::in};
-    decoder dec{stream};
-
-    std::size_t abi_version;
-    dec.decode(abi_version);
-
-    if(abi_version != finder_abi_version) {
-        std::cerr << "error: cache was build by a different version of finder\n";
-        std::exit(EXIT_FAILURE);
-    }
 
     return stream; // CE
 }
@@ -72,7 +208,7 @@ void rebuild_cache(fs::path directory) {
         db.add(path);
     }
 
-    std::fstream output_stream = open_database_output_stream(directory);
+    std::fstream output_stream{directory / finder_cache_path, std::ios::out};
     db.marshall(output_stream);
 }
 
@@ -82,7 +218,7 @@ void locate_exact_filename(fs::path filename, fs::path directory) {
         std::exit(EXIT_FAILURE);
     }
 
-    auto input_stream = open_database_input_stream(directory);
+    auto input_stream = open_database_stream(directory);
 
     lazy_database db{input_stream};
     for(const auto &path : db.locate(filename)) {
@@ -92,9 +228,16 @@ void locate_exact_filename(fs::path filename, fs::path directory) {
 }
 
 void list_all_filepaths(fs::path directory) {
-    auto input_stream = open_database_input_stream(directory);
-
+    auto input_stream = open_database_stream(directory);
     decoder dec{input_stream};
+
+    std::size_t abi_version;
+    dec.decode(abi_version);
+
+    if(abi_version != finder_abi_version) {
+        std::cerr << "error: cache was build by a different version of finder\n";
+        std::exit(EXIT_FAILURE);
+    }
 
     std::size_t count;
     dec.decode(count);
